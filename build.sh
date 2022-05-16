d40e9bef10e6af3f44a3130139d2af0227575f78
51pwn <18223385+hktalent@users.noreply.github.com>
2022-05-16 22:06:47 +0800

    up

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 rm -rf dist
+rm -rf hktdist
 XX1=`pwd`
 cd ~/MyWork/hackerToolsApp/app
+yarn install
 yarn build
 mv dist $XX1/
+git commit -m "up" .
+git push
+
+cd ~/MyWork/Hack-Tools
+yarn install
+yarn build
+mv dist $XX1/hktdist
+git commit -m "up" .
+git push
+
 cd $XX1
 go get -u
 go mod tidy
 go build main.go
 git commit -m "up" .
 git push
-
-
